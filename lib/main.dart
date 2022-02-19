971720a09ccfd13301ff32332820d49d894de8ab
ahmedmohab96 <ahmdmhabhbyb@gmail.com>
2022-02-19 02:36:33 +0200

    https://github.com/ahmedmohab96/Design-Workshop_app.git

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:workshop_app/constants.dart';
+import 'package:workshop_app/screens/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Design Workshop',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: primaryColor,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.all(defaultPadding * 1.5),
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white12,
+          border: textFieldOutlineInputBorder,
+          enabledBorder: textFieldOutlineInputBorder,
+          focusedBorder: textFieldOutlineInputBorder,
+          hintStyle: TextStyle(color: Colors.white30),
+        ),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/screens/auth/sign_in_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:workshop_app/constants.dart';
+import 'components/sign_in_form.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.all(defaultPadding),
+          child: Column(
+            children: [
+              SizedBox(height: defaultPadding),
+              Text(
+                "Welcome Back!",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5!
+                    .copyWith(fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "Please sign into your account",
+                style: TextStyle(color: Colors.white30, height: 1.8),
+              ),
+              Spacer(),
+              SignInForm(),
+              SizedBox(height: defaultPadding),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(backgroundColor: Colors.white),
+                  icon: SvgPicture.asset("assets/icons/google.svg"),
+                  label: Text(
+                    "Sign in with Google",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don’t have an account?"),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
